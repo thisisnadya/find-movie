8332f10ac076d2f5a2cd108a85dab33d0fc0e6e2
nadyamumtazah <tasyamumtazah5@gmail.com>
2022-01-13 17:21:50 +0700

    add search feature

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const movieContainer = document.querySelector('.movie-container');
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
+
+
+const form = document.querySelector('.form');
+const searchInput = document.getElementById('search');
+
 
 getMovies(API_URL)
 function getMovies(url) {
@@ -20,10 +26,24 @@ function showMovies(data) {
                         <img src="${IMG_URL}${element.poster_path}" class="card-img-top img-fluid" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"><strong>${element.title}</strong></h5>
-                            <p class="card-text text-muted">2016</p>
+                            <p class="card-text text-muted">${element.vote_average}</p>
                         </div>
+                        <a class="show-details" href="#">Show Details</a> 
                     </div>
-                </div>`
+                </div>`;
         movieContainer.innerHTML = movie;
     });
 }
+const showDetailsButton = document.querySelector('.show-details');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchInputValue = searchInput.value;
+
+    if(searchInputValue){
+        getMovies(SEARCH_URL+'&query='+searchInputValue);
+    }
+});
+
