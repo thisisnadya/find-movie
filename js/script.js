dfedcd2020e391936977456429a0e2c42dab6d76
nadyamumtazah <tasyamumtazah5@gmail.com>
2022-01-18 11:03:47 +0700

    add icon and show modal movie details

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 const API_KEY = 'api_key=9a49f607eea2d650a495df31e926d29f';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
-const movieContainer = document.querySelector('.movie-container');
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
 
-
+const movieContainer = document.querySelector('.movie-container');
 const form = document.querySelector('.form');
 const searchInput = document.getElementById('search');
 
@@ -25,23 +24,46 @@ function showMovies(data) {
                     <div class="card">
                         <img src="${IMG_URL}${element.poster_path}" class="card-img-top img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title"><strong>${element.title}</strong></h5>
-                            <p class="card-text text-muted">${element.vote_average}</p>
+                            <h5 class="card-title show-details" data-bs-toggle="modal" data-bs-target="#exampleModal" data-tmdb="${element.id}">${element.title}</h5>
+                            <p class="card-text text-muted">${element.vote_average} <i class="bi bi-star-fill"></i></p>
                         </div>
-                        <a class="show-details" href="#">Show Details</a> 
                     </div>
                 </div>`;
         movieContainer.innerHTML = movie;
     });
 }
-const showDetailsButton = document.querySelector('.show-details');
+document.addEventListener('click', function(e) {
+    console.log(e.target);
+    if(e.target.classList.contains('show-details')){
+        const movieID = e.target.dataset.tmdb;
+        fetch('https://api.themoviedb.org/3/movie/'+ movieID + '?' + API_KEY)
+            .then(res => res.json())
+            .then(res => {
+                const movieDetail = `<div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <img src="${IMG_URL}${res.poster_path}" class="img-fluid" alt="">
+                                            </div>
+                                            <div class="col-md">
+                                                <ul class="list-group">
+                                                    <li class="list-group-item"><h6><strong>${res.title}</strong></h6></li>
+                                                    <li class="list-group-item"><strong>Release Date : </strong>${res.release_date}</li>
+                                                    <li class="list-group-item"><strong>Overview : </strong>${res.overview}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>`
+                const modalBody = document.querySelector('.modal-body');
+                modalBody.innerHTML = movieDetail;
+                console.log(res.title);
+            });
+    }
+});
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const searchInputValue = searchInput.value;
-
+    let searchInputValue = searchInput.value;
     if(searchInputValue){
         getMovies(SEARCH_URL+'&query='+searchInputValue);
     }
